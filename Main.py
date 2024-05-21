6e9af077c3bcd6a950c910e08d32bac84c6865f3
MapsMakeStudios <mapsmakestudios@gmail.com>
2024-05-21 13:48:54 +1000

    Version 0.0.2, This new version adds support for finding basic random positions for each player on the attacking team.

--- Main.py
+++ Main.py
@@ -4,9 +4,44 @@ from time import sleep
 gridSizeX = 1000
 gridSizeY = 1000
 
-def spawn_enemies(amount):
-    def Enemy(enemyNum):
-        enemySpawnRadiusMaxX = gridSizeX - 700
-        enemyPos1X = randint(0, enemySpawnRadiusMaxX)
+attackSpawnRadiusMaxX = gridSizeX - 700
+attackSpawnRadiusMaxY = gridSizeY - 600
+
+
+def attackEnemy1():
+    attackPos1X = randint(0, attackSpawnRadiusMaxX)
+    attackPos1Y = randint(0, attackSpawnRadiusMaxY)
+    print("Attack Player 1, X - " + str(attackPos1X) + ", Y - " + str(attackPos1Y))
+def attackEnemy2():
+    attackPos2X = randint(0, attackSpawnRadiusMaxX)
+    attackPos2Y = randint(0, attackSpawnRadiusMaxY)
+    print("Attack Player 2, X - " + str(attackPos2X) + ", Y - " + str(attackPos2Y))
+def attackEnemy3():
+    attackPos3X = randint(0, attackSpawnRadiusMaxX)
+    attackPos3Y = randint(0, attackSpawnRadiusMaxY)
+    print("Attack Player 3, X - " + str(attackPos3X) + ", Y - " + str(attackPos3Y))
+def attackEnemy4():
+    attackPos4X = randint(0, attackSpawnRadiusMaxX)
+    attackPos4Y = randint(0, attackSpawnRadiusMaxY)
+    print("Attack Player 4, X - " + str(attackPos4X) + ", Y - " + str(attackPos4Y))
+def attackEnemy5():
+    attackPos5X = randint(0, attackSpawnRadiusMaxX)
+    attackPos5Y = randint(0, attackSpawnRadiusMaxY)
+    print("Attack Player 5, X - " + str(attackPos5X) + ", Y - " + str(attackPos5Y))
+
+print("Printing Debug text")
+sleep(5)
+attackEnemy1()
+sleep(1)
+attackEnemy2()
+sleep(1)
+attackEnemy3()
+sleep(1)
+attackEnemy4()
+sleep(1)
+attackEnemy5()
+
+
+
 
 
