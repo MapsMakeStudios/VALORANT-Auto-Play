1f1bb78cc7dd2b8af87bda317e6fdb835f6858b0
MapsMakeStudios <mapsmakestudios@gmail.com>
2024-05-21 12:26:29 +1000

    Version 0.0.1, this is a test commit and test.

---
+++ Main.py
@@ -0,0 +1,11 @@
+from random import randint
+from time import sleep
+
+gridSizeX = 1000
+gridSizeY = 1000
+
+def spawn_enemies(amount):
+    def Enemy(enemyNum):
+        enemySpawnRadiusMaxX = gridSizeX - 700
+        enemyPos1X = randint(0, enemySpawnRadiusMaxX)
+
