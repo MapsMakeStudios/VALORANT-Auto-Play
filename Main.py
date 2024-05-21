7385ce79fbc619dc04d821340a72e9f050da541a
MapsMakeStudios <mapsmakestudios@gmail.com>
2024-05-21 16:22:35 +1000

    Version 0.0.3, This version adds extra features and a lot of debugging such as, - Adding Defense attack starting positions - Fixing problems with incorrect bounding boxes of start phase areas for the different sides - Fixed Variable misplacement

--- Main.py
+++ Main.py
@@ -4,42 +4,84 @@ from time import sleep
 gridSizeX = 1000
 gridSizeY = 1000
 
-attackSpawnRadiusMaxX = gridSizeX - 700
+attackSpawnRadiusMaxX = gridSizeX
 attackSpawnRadiusMaxY = gridSizeY - 600
+attackSpawnRadiusMinX = gridSizeX - gridSizeX
+attackSpawnRadiusMinY = gridSizeY - gridSizeY
+defenseSpawnRadiusMaxX = gridSizeX
+defenseSpawnRadiusMaxY = gridSizeY
+defenseSpawnRadiusMinX = gridSizeX - gridSizeX
+defenseSpawnRadiusMinY = gridSizeY - 300
 
-
-def attackEnemy1():
-    attackPos1X = randint(0, attackSpawnRadiusMaxX)
-    attackPos1Y = randint(0, attackSpawnRadiusMaxY)
+def attackPlayer1():
+    attackPos1X = randint(attackSpawnRadiusMinX, attackSpawnRadiusMaxX)
+    attackPos1Y = randint(attackSpawnRadiusMinY, attackSpawnRadiusMaxY)
     print("Attack Player 1, X - " + str(attackPos1X) + ", Y - " + str(attackPos1Y))
-def attackEnemy2():
-    attackPos2X = randint(0, attackSpawnRadiusMaxX)
-    attackPos2Y = randint(0, attackSpawnRadiusMaxY)
+def attackPlayer2():
+    attackPos2X = randint(attackSpawnRadiusMinX, attackSpawnRadiusMaxX)
+    attackPos2Y = randint(attackSpawnRadiusMinY, attackSpawnRadiusMaxY)
     print("Attack Player 2, X - " + str(attackPos2X) + ", Y - " + str(attackPos2Y))
-def attackEnemy3():
-    attackPos3X = randint(0, attackSpawnRadiusMaxX)
-    attackPos3Y = randint(0, attackSpawnRadiusMaxY)
+def attackPlayer3():
+    attackPos3X = randint(attackSpawnRadiusMinX, attackSpawnRadiusMaxX)
+    attackPos3Y = randint(attackSpawnRadiusMinY, attackSpawnRadiusMaxY)
     print("Attack Player 3, X - " + str(attackPos3X) + ", Y - " + str(attackPos3Y))
-def attackEnemy4():
-    attackPos4X = randint(0, attackSpawnRadiusMaxX)
-    attackPos4Y = randint(0, attackSpawnRadiusMaxY)
+def attackPlayer4():
+    attackPos4X = randint(attackSpawnRadiusMinX, attackSpawnRadiusMaxX)
+    attackPos4Y = randint(attackSpawnRadiusMinY, attackSpawnRadiusMaxY)
     print("Attack Player 4, X - " + str(attackPos4X) + ", Y - " + str(attackPos4Y))
-def attackEnemy5():
-    attackPos5X = randint(0, attackSpawnRadiusMaxX)
-    attackPos5Y = randint(0, attackSpawnRadiusMaxY)
+def attackPlayer5():
+    attackPos5X = randint(attackSpawnRadiusMinX, attackSpawnRadiusMaxX)
+    attackPos5Y = randint(attackSpawnRadiusMinY, attackSpawnRadiusMaxY)
     print("Attack Player 5, X - " + str(attackPos5X) + ", Y - " + str(attackPos5Y))
 
-print("Printing Debug text")
+def defensePlayer1():
+    defensePos1X = randint(defenseSpawnRadiusMinX, defenseSpawnRadiusMaxX)
+    defensePos1Y = randint(defenseSpawnRadiusMinY, defenseSpawnRadiusMaxY)
+    print("Attack Player 1, X - " + str(defensePos1X) + ", Y - " + str(defensePos1Y))
+def defensePlayer2():
+    defensePos2X = randint(defenseSpawnRadiusMinX, defenseSpawnRadiusMaxX)
+    defensePos2Y = randint(defenseSpawnRadiusMinY, defenseSpawnRadiusMaxY)
+    print("Attack Player 2, X - " + str(defensePos2X) + ", Y - " + str(defensePos2Y))
+def defensePlayer3():
+    defensePos3X = randint(defenseSpawnRadiusMinX, defenseSpawnRadiusMaxX)
+    defensePos3Y = randint(defenseSpawnRadiusMinY, defenseSpawnRadiusMaxY)
+    print("Attack Player 3, X - " + str(defensePos3X) + ", Y - " + str(defensePos3Y))
+def defensePlayer4():
+    defensePos4X = randint(defenseSpawnRadiusMinX, defenseSpawnRadiusMaxX)
+    defensePos4Y = randint(defenseSpawnRadiusMinY, defenseSpawnRadiusMaxY)
+    print("Attack Player 4, X - " + str(defensePos4X) + ", Y - " + str(defensePos4Y))
+def defensePlayer5():
+    defensePos5X = randint(defenseSpawnRadiusMinX, defenseSpawnRadiusMaxX)
+    defensePos5Y = randint(defenseSpawnRadiusMinY, defenseSpawnRadiusMaxY)
+    print("Attack Player 5, X - " + str(defensePos5X) + ", Y - " + str(defensePos5Y))
+
+
+print("Printing Attack Positions")
 sleep(5)
-attackEnemy1()
+attackPlayer1()
 sleep(1)
-attackEnemy2()
+attackPlayer2()
 sleep(1)
-attackEnemy3()
+attackPlayer3()
 sleep(1)
-attackEnemy4()
+attackPlayer4()
 sleep(1)
-attackEnemy5()
+attackPlayer5()
+sleep(1)
+print("Printing Defense Positions")
+sleep(5)
+defensePlayer1()
+sleep(1)
+defensePlayer2()
+sleep(1)
+defensePlayer3()
+sleep(1)
+defensePlayer4()
+sleep(1)
+defensePlayer5()
+sleep(1)
+
+
 
 
 
